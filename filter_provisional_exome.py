170514ccf13c7b3191c453b73200931274beb4d0
jelber2 <jelber2@lsu.edu>
2016-11-22 22:07:04 -0600

    added input and output arguments

--- filter_provisional_exome.py
+++ filter_provisional_exome.py
@@ -1,20 +1,21 @@
 #! /usr/bin/env python
+# encoding: utf-8
 
 ###############################################################################
 #
 # "filter_provisional_exome.py" Python script for GO2TR
 # created by Jean P. Elbers
 # jean.elbers@gmail.com
-# last edited 10 November 2014
+# last edited 22 November 2016
 #
 ###############################################################################
 #
 # Program steps:
 # -------------
 # 1.Input is provisional_exome.txt in the format:
-#   chrom start end strand	mRNAaccessionIdentifier
-#   NW_004848299.1 261683 261849 +	XM_005278413.1
-#   NW_004848299.1 266496 266605 +	XM_005278413.1
+#   chrom start end strand    mRNAaccessionIdentifier
+#   NW_004848299.1 261683 261849 +    XM_005278413.1
+#   NW_004848299.1 266496 266605 +    XM_005278413.1
 #
 # 2.Reads through each line of provisional_exome.txt and makes a list of lists
 #   called rawlist with the elements being each line, which is a list of
@@ -50,103 +51,152 @@
 #
 ###############################################################################
 
-# creates rawlist and InFile
-rawlist = []
-InFile = open('provisional_exome.txt', 'r')
-
-# read through each line in the file one by one and split the each
-# line into a list of two elements, the first (element 1), and the second
-# (element 2)
-# if multiple transcription accessions are present in the input
-# (ex: NW_004848299.1 432018 432084 +	XM_005294952.1;XM_005278436.1),
-# then the ';' is converted to a tab and a third list element is created
-for line in InFile:
-	if len(rawlist) < 1:
-		x = line.strip('\n').replace(';','\t').split('\t')
-		rawlist.append(x)
-	# if the first line of the input file has already been added to the
-	# list (i.e., rawlist[0] exists), then add the next line to the list
-	else:
-		y = line.strip('\n').replace(';','\t').split('\t')
-		rawlist.append(y)
-InFile.close()
-#uncomment for debugging
-#print rawlist
-
-# if a list element contains more than two items (i.e., rawlist[0][2] exists),
-# then split the element until there are only two items
-Element = 0
-rawlist2 = []
-for element in rawlist:
-	Subelement = 0
-	for subelement in element:
-		newsubelement1 = ''
-		newsubelement2 = ''
-		newelement = ''
-		newlist = []
-		newsubelement1 = rawlist[Element][0]
-		if Subelement + 1 < len(rawlist[Element]):
-			Subelement+=1
-			newsubelement2 = rawlist[Element][Subelement]
-			newelement = newsubelement1 + '\t' + newsubelement2
-			newlist = newelement.split('\t')
-			rawlist2.append(newlist)
-		else:
-			if Element + 1 < len(rawlist):
-				Element+=1
-#uncomment for debugging
-#print rawlist2
-
-# imports defaultdict from module collections in order to have
-# a dictionary of "lists"
+import os
+import argparse
+# imports defaultdict from module collections in order to have a dictionary
+# of "lists"
 from collections import defaultdict
 
-# read through each element of the list (note: each element is actually
-# a two item list the first being chrom start end and the
-# second being mRNAaccessionIdentifier) and make each list element a dictionary
-# entry with chrom start end as the key with its associated
-# mRNAaccessionIdentifier as a value
-# NOTE: The defaultdict option allows for the keys and values to be lists
-rawdict = defaultdict(list)
-for chrom, mRNAaccessionIdentifier in rawlist2:
-	rawdict[chrom].append(mRNAaccessionIdentifier)
-#uncomment for debugging
-#print rawdict
+class FullPaths(argparse.Action):
+    """Expand user- and relative-paths"""
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, os.path.abspath(os.path.expanduser(values)))
 
-# makes outfile
-OutFileName='GO2TR.coords.txt'
-OutFile = open(OutFileName, 'w')
+def is_dir(dirname):
+    if not os.path.isdir(dirname):
+        msg = "{0} is not a directory".format(dirname)
+        raise argparse.ArgumentTypeError(msg)
+    else:
+        return dirname
 
-# converts all values (not keys) in rawdict into a set
-dictvalues = rawdict.viewvalues()
-dictvalues = set(str(rawdict.viewvalues()).replace('[','').replace(']','')\
-.replace("'","").replace(';',' ').replace(',','').split())
-#uncomment for debugging
-#print dictvalues
+def get_args():
+    """Get arguments from CLI"""
+    parser = argparse.ArgumentParser(
+            description="""\nFilters exome by retained mRNA list and returns target region""")
+    parser.add_argument(
+            "--exome",
+            required=True,
+            action=FullPaths,
+            help="""The provisional exome to analyze"""
+        )
+    parser.add_argument(
+            "--mRNA",
+            required=True,
+            action=FullPaths,
+            help="""The retained mRNA list to filter the exome by"""
+        )
+    parser.add_argument(
+            "--out",
+            action=FullPaths,
+            default="GO2TR.coords.txt",
+            help="""Name of target region to output (default = GO2TR.coords.txt)"""
+        )
+    return parser.parse_args()
 
-# opens retained_mRNA_list.txt and creates inputlist
-f = open('retained_mRNA_list.txt', 'r')
-inputlist = []
+def main():
+    args = get_args()
+    # creates rawlist and InFile
+    rawlist = []
+    exome = args.exome
+    InFile = open(exome, 'r')
 
-# reads through each line in the input file one by one and stores
-# each line in a list
-for line in f:
-	x = line.strip('\n')
-	inputlist.append(x)
-f.close()
-#uncomment for debugging
-#print inputlist
+    # read through each line in the file one by one and split the each
+    # line into a list of two elements, the first (element 1), and the second
+    # (element 2)
+    # if multiple transcription accessions are present in the input
+    # (ex: NW_004848299.1 432018 432084 +    XM_005294952.1;XM_005278436.1),
+    # then the ';' is converted to a tab and a third list element is created
+    for line in InFile:
+        if len(rawlist) < 1:
+            x = line.strip('\n').replace(';','\t').split('\t')
+            rawlist.append(x)
+        # if the first line of the input file has already been added to the
+        # list (i.e., rawlist[0] exists), then add the next line to the list
+        else:
+            y = line.strip('\n').replace(';','\t').split('\t')
+            rawlist.append(y)
+    InFile.close()
+    #uncomment for debugging
+    #print rawlist
 
-# searches within dictvalues for matching inputvalues and saves
-# result as the set matches
-matches = set(inputlist).intersection(dictvalues)
-#uncomment for debugging
-#print matches
+    # if a list element contains more than two items (i.e., rawlist[0][2] exists),
+    # then split the element until there are only two items
+    Element = 0
+    rawlist2 = []
+    for element in rawlist:
+        Subelement = 0
+        for subelement in element:
+            newsubelement1 = ''
+            newsubelement2 = ''
+            newelement = ''
+            newlist = []
+            newsubelement1 = rawlist[Element][0]
+            if Subelement + 1 < len(rawlist[Element]):
+                Subelement+=1
+                newsubelement2 = rawlist[Element][Subelement]
+                newelement = newsubelement1 + '\t' + newsubelement2
+                newlist = newelement.split('\t')
+                rawlist2.append(newlist)
+            else:
+                if Element + 1 < len(rawlist):
+                    Element+=1
+    #uncomment for debugging
+    #print rawlist2
 
-# filters rawdict so that only entries with values that correspond to
-# matches are returned but outputs the genomic coordinates (i.e., the keys)
-for key, value in rawdict.items():
-	if value[0] in matches:
-		OutFile.write(key)
-		OutFile.write('\n')
-OutFile.close()
+    # imports defaultdict from module collections in order to have
+    # a dictionary of "lists"
+    from collections import defaultdict
+
+    # read through each element of the list (note: each element is actually
+    # a two item list the first being chrom start end and the
+    # second being mRNAaccessionIdentifier) and make each list element a dictionary
+    # entry with chrom start end as the key with its associated
+    # mRNAaccessionIdentifier as a value
+    # NOTE: The defaultdict option allows for the keys and values to be lists
+    rawdict = defaultdict(list)
+    for chrom, mRNAaccessionIdentifier in rawlist2:
+        rawdict[chrom].append(mRNAaccessionIdentifier)
+    #uncomment for debugging
+    #print rawdict
+
+    # makes outfile
+    OutFileName=args.out
+    OutFile = open(OutFileName, 'w')
+
+    # converts all values (not keys) in rawdict into a set
+    dictvalues = rawdict.viewvalues()
+    dictvalues = set(str(rawdict.viewvalues()).replace('[','').replace(']','')\
+    .replace("'","").replace(';',' ').replace(',','').split())
+    #uncomment for debugging
+    #print dictvalues
+
+    # opens retained_mRNA_list.txt and creates inputlist
+    mRNA = args.mRNA
+    f = open(mRNA, 'r')
+    inputlist = []
+
+    # reads through each line in the input file one by one and stores
+    # each line in a list
+    for line in f:
+        x = line.strip('\n')
+        inputlist.append(x)
+    f.close()
+    #uncomment for debugging
+    #print inputlist
+
+    # searches within dictvalues for matching inputvalues and saves
+    # result as the set matches
+    matches = set(inputlist).intersection(dictvalues)
+    #uncomment for debugging
+    #print matches
+
+    # filters rawdict so that only entries with values that correspond to
+    # matches are returned but outputs the genomic coordinates (i.e., the keys)
+    for key, value in rawdict.items():
+        if value[0] in matches:
+            OutFile.write(key)
+            OutFile.write('\n')
+    OutFile.close()
+
+if __name__ == '__main__':
+    main()
